79d385921ffef789421c955363f3983e5f495b71
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-04 11:31:24 -0300

    new js slide loja

---
+++ public/javascript/loja-slides.js
@@ -0,0 +1,21 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+const productSwiper = new Swiper('.product-swiper', {
+  direction: 'horizontal',
+  loop: false,
+  grabCursor: true,
+  spaceBetween: 30,
+  slidesPerView: 'auto',
+  centeredSlides: false,
+  slidesOffsetBefore: 0,
+  slidesOffsetAfter: 0,
+  navigation: {
+    nextEl: '.loja-swiper-button-next',
+    prevEl: '.loja-swiper-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 40,
+    },
+  },
+});
