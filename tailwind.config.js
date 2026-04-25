4aa09f9a5516bb69a18d97c2f7c1512630e06df1
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-24 21:59:21 -0300

    new animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,15 @@ module.exports = {
             shadow: 'rgba(0, 0, 0, 0.25)',
             shadowItem: 'rgba(0, 0, 0, 0.5)',
         },
+        keyframes: {
+            'fade-top': {
+                '0%': { 'transform': 'translateY(-20px)', 'opacity': 0 },
+                '100%': { 'transform': 'translateY(0)', 'opacity': 1 }
+            }
+        },
+        animation: {
+            'fadeTop': 'fade-top 0.5s forwards'
+        },
         extend: {
             fontFamily: {
                 sans: ['Montserrat', 'sans-serif'],
