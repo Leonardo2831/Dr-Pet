ed985aea352724f6e93701fc1b062c1d892a0a7a
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-27 20:37:05 -0300

    change to function

---
+++ public/javascript/loja/slides-produtos-loja.js
@@ -0,0 +1,29 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+export default function slidesLoja(){
+  const productSwiper = new Swiper('.product-swiper', {
+    direction: 'horizontal',
+    loop: false,
+    grabCursor: true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    slidesOffsetBefore: 0,
+    slidesOffsetAfter: 0,
+    navigation: {
+      nextEl: '.loja-swiper-button-next',
+      prevEl: '.loja-swiper-button-prev',
+    },
+    breakpoints: {
+      480: {
+        spaceBetween: 20,
+      },
+      768: {
+        spaceBetween: 30,
+      },
+      1024: {
+        spaceBetween: 40,
+      },
+    },
+  });
+}
