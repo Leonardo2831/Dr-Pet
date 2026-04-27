995dce4469705ccfa34ca91cbf8543d91f0a1220
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-27 20:36:53 -0300

    chante to function

---
+++ public/javascript/home/slides-home.js
@@ -0,0 +1,20 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+
+export default function slidesHome() {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
