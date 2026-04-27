d6d2ff13de77cfbce5358ce6068cb85e5d3ef62b
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-27 15:36:10 -0300

    added menuUser

--- public/javascript/home/script-home.js
+++ public/javascript/home/script-home.js
@@ -4,4 +4,5 @@ import slidesHome from "../slides/slides-home.js";
 document.addEventListener('DOMContentLoaded', () => {
     slidesHome();
     const menuMobileHeader = new PopUp('[data-button="menuMobile"]', '[data-menuMobile]').init();
+    const menuUser = new PopUp('[data-menuUser]', '[data-modal="menuUser"]').init();
 });
