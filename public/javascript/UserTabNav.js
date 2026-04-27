1f5c50b5dbbe4e2e99681ee7ac0cc7d40b687549
Rafael Cili <rafaelgcili2019@gmail.com>
2026-04-27 17:13:11 -0300

    JS e HTML User settings

---
+++ public/javascript/UserTabNav.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll('[data-target]');
+
+const sections = document.querySelectorAll('section[id]');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        sections.forEach((section) => {
+            section.classList.add('hidden');
+        });
+        
+        const linkCorreto = link.dataset.target;
+        document.getElementById(linkCorreto).classList.remove('hidden');
+    })
+})
+
+
