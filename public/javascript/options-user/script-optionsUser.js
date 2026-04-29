6fbf7a5f9b34a05512cf6883558b31ea02c6fee0
Leonardo2831 <leoferraz1818@gmail.com>
2026-04-29 14:51:17 -0300

    new class to menu options address

--- public/javascript/options-user/script-optionsUser.js
+++ public/javascript/options-user/script-optionsUser.js
@@ -2,4 +2,5 @@ import MenuOptions from "./modal-menuOptions.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuOptionsPets = new MenuOptions('[data-button="options-pet"]', '[data-popup="options-pet"]');
+    const menuOptionsAddress = new MenuOptions('[data-button="options-address"]', '[data-popup="options-address"]');
 });
