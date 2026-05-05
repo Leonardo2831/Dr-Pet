b34b6b3a759906f55fd8d96524af4c768c874288
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-05 15:59:30 -0300

    init code to create slide in adm page

---
+++ public/javascript/administrador/components/structSlide.js
@@ -0,0 +1,24 @@
+import Fetch from "../../Fetch.js";
+
+export default function structSlide(object){
+    const figure = document.createElement('figure');
+    figure.className = "relative w-[280px] h-[148px] rounded-[10px] shrink-0 bg-cover bg-center bg-no-repeat";
+    figure.setAttribute('data-content-slides', object.id);
+    figure.style.backgroundImage = `url('${object.url}')`;
+
+    figure.innerHTML = `
+        <div class="absolute inset-0 bg-black/25 rounded-[10px]"></div>
+        <button type="button" class="absolute -top-[12px] -right-[12px] flex items-center justify-center p-3 bg-gray-200 rounded-full hover:bg-gray-300 transition-colors z-10 shadow-sm">
+            <img src="../images/icons/administrador/delete-slide.svg" alt="Remover slide" class="w-5 h-5">
+        </button>
+    `;
+
+    const button = figure.querySelector('button');
+    button.addEventListener('click', () => {
+        const fetch = new Fetch(`http://localhost:3000/api/slides/${object.id}`);
+        fetch.delete();
+        figure.remove();
+    });
+
+    return figure;
+}
