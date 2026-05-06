1494f270f76f8f08f576517659c9f6c31e132cc3
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-05 22:34:01 -0300

    change struct slide link in class fetch

--- public/javascript/administrador/components/structSlide.js
+++ public/javascript/administrador/components/structSlide.js
@@ -4,7 +4,8 @@ export default function structSlide(object){
     const figure = document.createElement('figure');
     figure.className = "relative w-[280px] h-[148px] rounded-[10px] shrink-0 bg-cover bg-center bg-no-repeat";
     figure.setAttribute('data-content-slides', object.id);
-    figure.style.backgroundImage = `url('${object.url}')`;
+    figure.setAttribute('aria-label', object.altImage || 'Slide image');
+    figure.style.backgroundImage = `url('${object.image}')`;
 
     figure.innerHTML = `
         <div class="absolute inset-0 bg-black/25 rounded-[10px]"></div>
@@ -15,7 +16,7 @@ export default function structSlide(object){
 
     const button = figure.querySelector('button');
     button.addEventListener('click', () => {
-        const fetch = new Fetch(`http://localhost:3000/api/slides/${object.id}`);
+        const fetch = new Fetch(`http://localhost:3000/api/slides/slides-home/${object.id}`);
         fetch.delete();
         figure.remove();
     });
