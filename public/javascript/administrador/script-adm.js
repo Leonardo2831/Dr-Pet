f39a8f60c65de31986f00e2dd40daed2a92d6061
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-06 15:33:36 -0300

    new schedule config

--- public/javascript/administrador/script-adm.js
+++ public/javascript/administrador/script-adm.js
@@ -1,7 +1,9 @@
 import selectSectionAdm from "./select-section-adm.js";
 import SlidesHome from "./Slides-home.js";
+import Agenda from "./Agenda.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     selectSectionAdm();
     const slidesConfig = new SlidesHome('[data-add-slide]', '[data-content-slides]').init();
+    const scheduleConfig = new Agenda('[data-content-schedule]', '[data-input-date="adm"]', '[data-select="service"]', '[data-button="searchSchedule"]').init();
 });
