086002c827b9d15f7395c28b04e23eb9bcfe8b58
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-05 22:33:51 -0300

    new slideshome

--- public/javascript/administrador/script-adm.js
+++ public/javascript/administrador/script-adm.js
@@ -1,5 +1,7 @@
 import selectSectionAdm from "./select-section-adm.js";
+import SlidesHome from "./Slides-home.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     selectSectionAdm();
+    const slidesConfig = new SlidesHome('[data-add-slide]', '[data-content-slides]').init();
 });
