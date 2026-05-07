ad8d6eb7809fba8ca573e665562e40653e9c83e6
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-06 22:41:24 -0300

    code to create struct products

--- public/javascript/administrador/components/structProducts.js
+++ public/javascript/administrador/components/structProducts.js
@@ -1,11 +1,31 @@
 export default function structProducts(object){
-    const div = document.createElement('div');
-    div.className = "flex items-center gap-4 p-4 bg-white rounded-lg shadow border border-slate-200 hover:shadow-md transition-shadow";
-    div.setAttribute('data-id', object.id);
+    const tr = document.createElement('tr');
+    tr.className = "border-b-[3px] border-gray-100 *:text-lg *:leading-[32px] *:text-gray-700 transition-colors";
+    tr.setAttribute('data-id', object.id);
 
-    div.innerHTML = `
-        
+    tr.innerHTML = `
+        <td class="max-w-0 truncate w-[25%]">${object.name}</td>
+        <td>${object.category}</td>
+        <td>${Number(object.price).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</td>
+        <td class="max-w-0 truncate w-[25%]">${object.shortDescription}</td>
+        <td class="max-w-0 truncate w-[25%]">${object.longDescription}</td>
+        <td class="py-5 text-center">
+            <button class="hover:opacity-80 transition-opacity mx-auto flex">
+                <img src="../images/icons/administrador/edit-blue.svg" alt="Editar" class="w-6 h-6 md:w-[35px] md:h-[38px]">
+            </button>
+        </td>
+        <td class="py-5 text-center">
+            <button data-button="deleteProduct" class="p-4 hover:opacity-80 transition-opacity mx-auto flex">
+                <img src="../images/icons/administrador/delete-product.svg" alt="Excluir" class="w-6 h-6 md:w-[37px] md:h-[38px]">
+            </button>
+        </td>
     `;
 
-    return div;
+    tr.querySelector('[data-button="deleteProduct"]').addEventListener('click', () => {
+        const fetchAPI = new Fetch('produtos', '[data-modal-info="adm"]');
+        fetchAPI.delete(object.id);
+        tr.remove();
+    });
+
+    return tr;
 }
