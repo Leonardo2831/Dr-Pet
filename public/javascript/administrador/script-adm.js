d5b94118324ef6496a8ed852dd26f84987edbeb4
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-06 22:41:31 -0300

    init config in script

--- public/javascript/administrador/script-adm.js
+++ public/javascript/administrador/script-adm.js
@@ -1,9 +1,11 @@
 import selectSectionAdm from "./select-section-adm.js";
-import SlidesHome from "./Slides-home.js";
+import SlidesHome from "./SlidesHome.js";
 import Agenda from "./Agenda.js";
+import Products from "./Products.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     selectSectionAdm();
     const slidesConfig = new SlidesHome('[data-add-slide]', '[data-content-slides]').init();
     const scheduleConfig = new Agenda('[data-content-schedule]', '[data-input-date="adm"]', '[data-select="service"]', '[data-button="searchSchedule"]').init();
+    const productsConfig = new Products('[data-content-products="table"]', '[data-button="addProduct"]', '[data-input="filterProduct"]', '[data-select="typeProduct"]').init();
 });
