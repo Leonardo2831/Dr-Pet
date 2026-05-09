e56738d5403abe3fee4de28a7e4af0e4c3fffe72
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-09 15:32:53 -0300

    new style to cards loja

--- public/javascript/loja/components/structProductLoja.js
+++ public/javascript/loja/components/structProductLoja.js
@@ -8,7 +8,7 @@ export default function structProduct(product) {
         <figure class="relative h-[160px] xs:h-[180px] sm:h-[220px] md:h-[300px] lg:h-[350px] overflow-hidden rounded-t-[10px]">
             <img src="${product.imageMain.image}" alt="${product.imageMain.alt}" class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
         </figure>
-        <div class="h-[170px] flex flex-col justify-between gap-2 flex-grow bg-gray-200 p-3 sm:p-4 md:p-6 rounded-b-[10px]">
+        <div class="h-[170px] flex flex-col justify-between gap-2 flex-grow bg-gray-200 p-3 sm:p-4 md:p-6 rounded-b-[10px] group-hover:bg-gray-300 transition-colors">
             <article class="flex flex-col gap-1 sm:gap-2">
                 <h3 class="text-gray-700 font-medium text-[14px] sm:text-[16px] md:text-[20px] leading-tight">
                     ${product.name}
