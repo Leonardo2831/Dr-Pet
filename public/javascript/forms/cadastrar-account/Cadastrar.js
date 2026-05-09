cea048bcaa6e9bcadf5e08ff4dfdcf8ac80da2cd
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-09 12:26:57 -0300

    change to form class to cadastrar new account

---
+++ public/javascript/forms/cadastrar-account/Cadastrar.js
@@ -0,0 +1,49 @@
+import Fetch from '../../Fetch.js';
+import toggleShowPassword from '../toggleShowPassword.js';
+
+export default class Cadastrar {
+    constructor(selectorForm) {
+        this.form = document.querySelector(selectorForm);
+        this.api = new Fetch('usuarios', '[data-modal-info="cadastrar"]');
+
+        this.formCadastrar = this.formCadastrar.bind(this);
+    }
+
+    async formCadastrar(event){
+        event.preventDefault();
+
+        const formData = new FormData(this.form);
+        // cria um objeto com os dados do formulário
+        const dadosUsuario = Object.fromEntries(formData.entries());
+
+        if (dadosUsuario.password !== dadosUsuario.confirmPassword) {
+            this.api.showModalError('As senhas não são iguais');
+            return;
+        }
+
+        // deletando o atributo do objeto de confirmação de senha
+        delete dadosUsuario.confirmPassword;
+        dadosUsuario.pets = [];
+        dadosUsuario.typeUser = 'comum';
+
+        const response = await this.api.post(dadosUsuario);
+
+        if (response && response.ok) {
+            this.api.showModalSuccess('Conta criada com sucesso!');
+            window.location.href = 'login.html';
+        }
+    }
+
+    addEventSubmit() {
+        this.form.addEventListener('submit', this.formCadastrar);
+    }
+
+    init(){
+        if(this.form){
+            this.addEventSubmit();
+            toggleShowPassword();
+        }
+
+        return this;
+    }
+}
