55f1d99df58f35eac98e9e132893474796b43e3e
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-08 22:36:02 -0300

    new code to cadastro

--- public/javascript/cadastro.js
+++ public/javascript/cadastro.js
@@ -12,17 +12,35 @@ formCadastro.addEventListener('submit', async (event) => {
 
     if (dadosUsuario.password !== dadosUsuario.confirmPassword) {
         apiUsuarios.showModalError('As senhas não são iguais');
+        alert("As senhas não sao iguais! ");
         const modalErr = document.getElementById('modal-info');
         return;
     }
     delete dadosUsuario.confirmPassword;
-
+    dadosUsuario.pets = [];
+    dadosUsuario.typeUser = "comum";
     const response = await apiUsuarios.post(dadosUsuario);
 
     if (response && response.ok) {
-        formCadastro.reset();
-        setTimeout(() => {
-            window.location.href = 'login.html';
-        }, 2000);
+        alert("Conta criada com sucesso! ");
+        window.location.href = 'login.html';
     }
+
+});
+
+const botaoSenhaMostrar = document.querySelectorAll('[data-toggle-password]');
+    botaoSenhaMostrar.forEach(button => {
+        button.addEventListener("click", () => {
+            const inputId = button.getAttribute('data-toggle-password');
+            const senhaInput = document.getElementById(inputId);
+            const iconImg = button.querySelector('img');
+
+            if(senhaInput.type === 'password') {
+                senhaInput.type = 'text';
+                console.log("senha visivel")
+            } else {
+                senhaInput.type = 'password';
+                console.log("senha invisivel")
+            }
+        });
 });
