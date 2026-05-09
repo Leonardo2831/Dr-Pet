09b322eb5070db22c21f6032cbb5ea51eccfb4ab
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-09 15:25:06 -0300

    new class login with methods to login

--- public/javascript/forms/login-account/Login.js
+++ public/javascript/forms/login-account/Login.js
@@ -1,3 +1,58 @@
+import Fetch from '../../Fetch.js';
+import toggleShowPassword from '../toggleShowPassword.js';
+
 export default class Login{
-    
+    constructor(selectorForm, selectorInputEmail, selectorInputPassword){
+        this.form = document.querySelector(selectorForm);
+        this.inputEmail = document.querySelector(selectorInputEmail);
+        this.inputPassword = document.querySelector(selectorInputPassword);
+
+        this.fetchJson = new Fetch('usuarios', '[data-modal-info="login"]');
+
+        this.loginAccount = this.loginAccount.bind(this);
+    }
+
+    markErrorInput(){
+        this.inputEmail.parentElement.classList.add('error');
+        this.inputPassword.parentElement.classList.add('error');
+
+        this.inputEmail.addEventListener('focus', () => {
+            this.inputEmail.parentElement.classList.remove('error');
+        });
+        this.inputPassword.addEventListener('focus', () => {
+            this.inputPassword.parentElement.classList.remove('error');
+        });
+    }
+
+    async loginAccount(event){
+        event.preventDefault();
+
+        const dados = await this.fetchJson.get();
+        // procura usuário válido
+        const usuarioValido = dados.find(user =>
+            user.email === this.inputEmail.value &&
+            user.password === this.inputPassword.value
+        );
+
+        if (usuarioValido) {
+            this.fetchJson.showModalSuccess('Login realizado com sucesso!');
+            window.location.href = '../../index.html';
+        } else {
+            this.fetchJson.showModalError(null, 'E-mail ou senha incorretos');
+            this.markErrorInput();
+        }
+    }
+
+    addEventForm(){
+        this.form.addEventListener('submit', this.loginAccount);
+    }
+
+    init(){
+        if(this.form && this.inputEmail && this.inputPassword){
+            toggleShowPassword();
+            this.addEventForm();
+        }
+
+        return this;
+    }
 }
