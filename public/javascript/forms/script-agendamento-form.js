6cd9b69de47b17272b357885ffcc0630050bc33a
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-09 12:27:23 -0300

    new files, change name and move to new folder

--- public/javascript/forms/script-agendamento-form.js
+++
@@ -1,8 +0,0 @@
-const parametrosDaUrl = new URLSearchParams(window.location.search);
-const servicoSelecionado = parametrosDaUrl.get('servico');
-if (servicoSelecionado) {
-    const radioCorrespondente = document.querySelector(`input[name="servico"][value="${servicoSelecionado}"]`);
-    if (radioCorrespondente) {
-        radioCorrespondente.checked = true;
-    }
-}
