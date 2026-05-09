7b5b2a80333c029047aee82417173b30ed2c9b04
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-09 17:59:21 -0300

    new code to menu loding

--- public/javascript/menu-user/MenuUserLoding.js
+++ public/javascript/menu-user/MenuUserLoding.js
@@ -1,7 +1,43 @@
 import Fetch from '../Fetch.js';
-
+import Storage from '../Storage.js';
+import structMenuUser from './components/structMenuUser.js';
+import PopUp from '../Pop-up.js';
 export default class MenuUserLoading{
-    constructor(){
+    constructor(selectorMenuUserContent){
+        this.menuUserContent = document.querySelector(selectorMenuUserContent);
+
+        this.fetchJson = new Fetch('usuarios', '[data-modal-info-menu="user"]');
+    }
+
+    addEventLogout(){
+        this.menuUserContent.querySelector('[data-button="logout"]').addEventListener('click', () => {
+            Storage.removeItemStorage('user-id');
+            window.location.href = '../login.html';
+        });
+    }
+
+    lodingContent(){
+        const idUser = Storage.getValueStorage('user-id');
+
+        if(idUser){
+            this.fetchJson.get(idUser).then((user) => {
+                this.menuUserContent.innerHTML = structMenuUser(user);
+                this.addEventLogout();
+            });
+        } else {
+            this.menuUserContent.innerHTML = structMenuUser();
+        }
+
+        new PopUp('[data-menuUser]', '[data-modal="menuUser"]').init();
+    }
+
+    init(){
+        console.log(this.menuUserContent);
         
+        if(this.menuUserContent){
+            this.lodingContent();
+        }
+
+        return this;
     }
 }
