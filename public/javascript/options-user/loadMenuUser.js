0161e76b26d6d4776a199a5a98920b6fd84a8834
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-11 15:52:50 -0300

    loding menuUser

---
+++ public/javascript/options-user/loadMenuUser.js
@@ -0,0 +1,33 @@
+import Storage from "../Storage.js";
+import Fetch from "../Fetch.js";
+import UserInfosChange from "./UserInfosChange.js";
+
+export default async function loadMenuUser(){
+    const userId = Storage.getValueStorage('user-id');
+    const fetchUser = new Fetch('usuarios', '[data-modal-info-menu="user"]');
+    await fetchUser.get(userId).then((userData) => {
+        const nameUser = document.querySelector('[data-info="nameUser"]');
+        const infoDataUser = {
+            email: document.querySelector('[data-info="email"]'),
+            phone: document.querySelector('[data-info="telefone"]'),
+            password: document.querySelector('[data-info="senha"]')
+        }
+
+        if(!userData) return;
+        if(userData.userName) nameUser.textContent = userData.userName;
+        if(userData.email) infoDataUser.email.textContent = userData.email;
+        if(userData.phone) infoDataUser.phone.textContent = userData.phone;
+        if(userData.password) infoDataUser.password.textContent = '*'.repeat(userData.password.length);
+
+        const userInfosChange = new UserInfosChange(
+            '[data-button="saveEmail"]',
+            '[data-button="saveTelefone"]',
+            '[data-button="saveSenha"]',
+            '[data-input="editEmail"]',
+            '[data-input="editTelefone"]',
+            '[data-input="editSenha"]',
+            userData,
+            fetchUser
+        ).init();
+    });
+}
