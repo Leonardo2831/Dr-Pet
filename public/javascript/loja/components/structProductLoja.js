6f14207bd57963af0873482f17564a41837c4004
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-13 20:24:56 -0300

    change values in card to style

--- public/javascript/loja/components/structProductLoja.js
+++ public/javascript/loja/components/structProductLoja.js
@@ -5,15 +5,15 @@ export default function structProduct(product) {
     a.setAttribute('href', `./produto.html?id=${product.id}`);
 
     a.innerHTML = `
-        <figure class="relative h-[160px] xs:h-[180px] sm:h-[220px] md:h-[300px] lg:h-[350px] overflow-hidden rounded-t-[10px]">
+        <figure class="relative aspect-square overflow-hidden shrink-0 rounded-t-[10px]">
             <img src="${product.imageMain.image}" alt="${product.imageMain.alt}" class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
         </figure>
-        <div class="h-[170px] flex flex-col justify-between gap-2 flex-grow bg-gray-200 p-3 sm:p-4 md:p-6 rounded-b-[10px] group-hover:bg-gray-300 transition-colors">
+        <div class="min-h-[200px] flex flex-col flex-grow justify-between gap-2 bg-gray-200 p-3 sm:p-4 md:p-6 rounded-b-[10px] group-hover:bg-gray-300 transition-colors">
             <article class="flex flex-col gap-1 sm:gap-2">
                 <h3 class="text-gray-700 font-medium text-[14px] sm:text-[16px] md:text-[20px] leading-tight">
                     ${product.name}
                 </h3>
-                <p class="text-gray-700 font-normal text-[12px] sm:text-[14px] md:text-[18px] leading-snug">
+                <p class="text-gray-700 font-normal text-base leading-relaxed line-clamp-2">
                     ${product.shortDescription}
                 </p>
             </article>
