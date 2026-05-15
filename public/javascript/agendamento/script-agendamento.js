e30ea645395effcf885d6b6abb9b6a9b812b553c
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-15 20:01:01 -0300

    change local attribute

--- public/javascript/agendamento/script-agendamento.js
+++ public/javascript/agendamento/script-agendamento.js
@@ -7,6 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuMobileHeader = new PopUp('[data-button="menuUserMobile"]', '[data-menu="header"]').init();
     const selectService = new PopUp('[data-button="selectService"]', '[data-modal="serviceSelected"]').init();
     selectAgenda(selectService);
-    const hours = new Hours('[data-content="hours-morning"]', '[data-content="hours-afternoon"]').init();
-    const calender = new Calender('[data-calender-title="month"]', '[data-calender-title="year"]', '[data-button="next-month"]', '[data-button="prev-month"]', '[data-container="calender-days"]', '[data-schedule-date="agenda"]', '[data-schedule-time="agenda"]', (date) => hours.loadByDay(date)).init();
+    const hours = new Hours('[data-content="hours-morning"]', '[data-content="hours-afternoon"]', '[data-schedule-time="agenda"]').init();
+    const calender = new Calender('[data-calender-title="month"]', '[data-calender-title="year"]', '[data-button="next-month"]', '[data-button="prev-month"]', '[data-container="calender-days"]', '[data-schedule-date="agenda"]', (date) => hours.loadByDay(date)).init();
 });
