0f18e3fca5a791368fe781c7d5ca736918af0c12
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-15 16:00:37 -0300

    init hour

--- public/javascript/agendamento/script-agendamento.js
+++ public/javascript/agendamento/script-agendamento.js
@@ -1,10 +1,12 @@
 import PopUp from './../Pop-up.js';
 import Calender from './Calender.js';
+import Hours from './Hours.js';
 import selectAgenda from './selectAgenda.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const menuMobileHeader = new PopUp('[data-button="menuUserMobile"]', '[data-menu="header"]').init();
     const selectService = new PopUp('[data-button="selectService"]', '[data-modal="serviceSelected"]').init();
     selectAgenda(selectService);
-    const calender = new Calender('[data-calender-title="month"]', '[data-calender-title="year"]', '[data-button="next-month"]', '[data-button="prev-month"]', '[data-container="calender-days"]', '[data-schedule-date="agenda"]', '[data-schedule-time="agenda"]').init();
+    const hours = new Hours('[data-content="hours-morning"]', '[data-content="hours-afternoon"]').init();
+    const calender = new Calender('[data-calender-title="month"]', '[data-calender-title="year"]', '[data-button="next-month"]', '[data-button="prev-month"]', '[data-container="calender-days"]', '[data-schedule-date="agenda"]', '[data-schedule-time="agenda"]', (date) => hours.loadByDay(date)).init();
 });
