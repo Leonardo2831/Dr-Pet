b0bf1ec5b8730010677cce4db9b8d92c386286cf
Leonardo2831 <leoferraz1818@gmail.com>
2026-05-15 20:00:52 -0300

    added in event change info in screen user

--- public/javascript/agendamento/components/structHours.js
+++ public/javascript/agendamento/components/structHours.js
@@ -1,8 +1,9 @@
-export default function structHours(hour){
+export default function structHours(hour, infoHour, period){
     const button = document.createElement('button');
     const span = document.createElement('span');
     button.setAttribute('type', 'button');
     button.classList.add('button-hour');
+    button.setAttribute('data-time-hour', hour);
     span.textContent = hour;
 
     button.addEventListener('click', () => {
@@ -11,6 +12,8 @@ export default function structHours(hour){
             buttonActive.classList.remove('active');
         }
         button.classList.add('active');
+        infoHour.textContent = `${button.getAttribute('data-time-hour')} ${period}`;
+        infoHour.setAttribute('data-time-schedule', button.getAttribute('data-time-hour'));
     });
     
     button.appendChild(span);
